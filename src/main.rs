41aec916de1a8505e0ad186fc4c2ae7bccd8a067
sakuraiyukina <sakuraiyukina0205@yahoo.co.jp>
2024-05-14 23:19:22 +0900

    Fixed call to help method.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 mod cli;
 
 use std::env;
-use crate::cli::print_help;
+use crate::cli::Help;
 
 fn main() {
 	let args: Vec<String> = env::args().collect();
 	if args.len() < 2 {
-		print_help("help");
+		Help::print_help("general");
 		return;
 	}
 	cli::parse_arguments(args);
