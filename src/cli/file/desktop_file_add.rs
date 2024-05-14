3f8e8a12d34b644b9d17e19064c4b58bd4c77946
sakuraiyukina <sakuraiyukina0205@yahoo.co.jp>
2024-05-14 23:17:39 +0900

    Add enumeration for file types and .desktop file's keys.

--- src/cli/file/desktop_file_add.rs
+++ src/cli/file/desktop_file_add.rs
@@ -1,13 +1,45 @@
-use crate::cli::print_help;
+use crate::cli::Help;
+
+enum FileType {
+	IsAppImage,
+	IsCommand,
+	IsExecutable,
+	IsScript,
+}
+
+enum FileKey {
+	Type            (String),
+	Version         (String),
+	Name            (String),
+	GenericName     (String),
+	NoDisplay       (bool),
+	Comment         (String),
+	Icon            (String),
+	Hidden          (String),
+	OnlyShowIn      (Vec<String>),
+	NotShowIn       (Vec<String>),
+	DBusActivatable (bool),
+	TryExec         (String),
+	Exec            (String),
+	Path            (String),
+	Terminal        (bool),
+	Actions         (Vec<String>),
+	MimeType        (Vec<String>),
+	Categories      (Vec<String>),
+	Keywords        (String),
+	StartupNotify   (bool),
+	StartupWMClass  (String),
+	URL             (String),
+}
 
 pub fn init(args: Vec<String>) {
 	if args.len() < 3 {
-		print_help("subcommand-add");
+		Help::print_help("subcommand-add");
 	} else {
-		file_create();
+		// file_create();
 	}
 }
 
-fn file_create() {
-	println!("Hello World");
+fn file_create(args: Vec<String>) {
+	// TODO;
 }
